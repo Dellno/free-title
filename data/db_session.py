a2b1e60ee0be1614758b20905e73994f5616552e
Dellno <89869798047t@gmail.com>
2024-04-05 17:40:31 +0500

    установка базы данных

---
+++ data/db_session.py
@@ -0,0 +1,28 @@
+import sqlalchemy as sa
+import sqlalchemy.orm as orm
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.declarative import declarative_base
+
+SqlAlchemyBase = declarative_base()
+
+__factory = None
+
+
+def global_init(db_file):
+    global __factory
+    if __factory:
+        return
+    if not db_file or not db_file.strip():
+        raise Exception("файлы базы даных отсутствуют, необходимо их указать.")
+    conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
+    print(f"Подключение к базе данных по адресу {conn_str}")
+    engine = sa.create_engine(conn_str, echo=False)
+    __factory = orm.sessionmaker(bind=engine)
+    from . import __all_models
+    SqlAlchemyBase.metadata.create_all(engine)
+    print(f"база данных {conn_str} подключена")
+
+
+def create_session() -> Session:
+    global __factory
+    return __factory()
