8f7df277af0f662b9ed88b615d611cda98c36e9f
dellno <89869798047t@gmail.com>
2024-04-08 20:37:17 +0500

    добавлен класс базы данных для пользователя и игры

---
+++ data/game.py
@@ -0,0 +1,16 @@
+import sqlalchemy
+import datetime
+from .db_session import SqlAlchemyBase
+from sqlalchemy import orm
+
+class Game(SqlAlchemyBase):
+    __tablename__ = 'games'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    icon_path = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    creator_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                sqlalchemy.ForeignKey("users.id"))
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
+    user = orm.relationship('User')
---
+++ data/user.py
@@ -0,0 +1,14 @@
+import sqlalchemy
+from sqlalchemy import orm
+from .db_session import SqlAlchemyBase
+from sqlalchemy import orm
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    login = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True, default=login)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    icon_path = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    news = orm.relationship("Game", back_populates='user')
