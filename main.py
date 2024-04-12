e80b141a26afdaf59a3eefa4d0a25dc52b9c44b9
Y_Ayaz <ayazrob2006@gmail.com>
2024-04-12 17:08:56 +0500

    изменен стиль css

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def reqister():
         user.set_password(form.password.data)
         db_sess.add(user)
         db_sess.commit()
-        return redirect('/login')
+        return redirect('/')
     return render_template('register.html', title='Регистрация', form=form)
 
 
