4fe421e330b986d53984d0b012714df6f5ab256f
Dellno <89869798047t@gmail.com>
2024-04-16 16:58:32 +0500

    добавлен обработчик входа, пользователь теперь может залогиниться

--- forms/login.py
+++ forms/login.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, TextAreaField, SubmitField
+from wtforms import PasswordField, StringField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
     login = StringField('Логин', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
