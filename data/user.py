1f565ed2f285a87240ed7974a0be105b428a1276
dellno <89869798047t@gmail.com>
2024-04-24 18:34:51 +0500

    исправлена ошибка нечитаемых стилей css

--- data/user.py
+++ data/user.py
@@ -4,6 +4,7 @@ from .db_session import SqlAlchemyBase
 from sqlalchemy import orm
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from time import time
 
 
 class User(SqlAlchemyBase, UserMixin):
