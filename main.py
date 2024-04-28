9afc3692a047a577fad78070f768bbc67f7308ae
dellno <89869798047t@gmail.com>
2024-04-28 13:37:37 +0500

    serve

--- main.py
+++ main.py
@@ -161,7 +161,7 @@ def main():
     db_session.global_init("db/game_and_user.db")
     app.register_blueprint(freeTitle_api.blueprint)
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    serve(app, host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
